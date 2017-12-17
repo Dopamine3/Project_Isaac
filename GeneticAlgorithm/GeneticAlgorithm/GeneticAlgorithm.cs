1f32d6dc1ae133784fa8b631b07c138909197e7f
Rikkerdaniel <rik.danka@gmail.com>
2017-12-17 18:50:38 +0100

    feat(Genetic algorithm): Genetic algorithm base class and propertis

---
+++ GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    public class GeneticAlgorithm<T>
+    {
+        public List<DNA<T>> Population { get; private set; }
+        public int Generation { get; private set; }
+        public float BestFitness { get; private set; }
+        public T[] BestGenes { get; private set; }
+    }
+}
