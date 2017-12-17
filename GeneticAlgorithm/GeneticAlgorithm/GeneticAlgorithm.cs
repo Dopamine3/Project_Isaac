03942a01e99e5520e8991e985ae37f6cf9936f36
Rikkerdaniel <rik.danka@gmail.com>
2017-12-17 18:54:30 +0100

    feat(Genetic algorithm): Genetic algorithm method

--- GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
+++ GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -12,5 +12,37 @@ namespace GeneticAlgorithm
         public int Generation { get; private set; }
         public float BestFitness { get; private set; }
         public T[] BestGenes { get; private set; }
+
+        public int Elitism;
+        public float MutationRate;
+
+        private List<DNA<T>> newPopulation;
+        private Random random;
+        private float fitnessSum;
+        private int dnaSize;
+        private Func<T> getRandomGene;
+        private Func<int, float> fitnessFunction;
+
+        public GeneticAlgorithm(int populationSize, int dnaSize, Random random, Func<T> getRandomGene, Func<int, float> fitnessFunction,
+        int elitism, float mutationRate = 0.01f)
+        {
+            Generation = 1;
+            Elitism = elitism;
+            MutationRate = mutationRate;
+            Population = new List<DNA<T>>(populationSize);
+            newPopulation = new List<DNA<T>>(populationSize);
+            this.random = random;
+            this.dnaSize = dnaSize;
+            this.getRandomGene = getRandomGene;
+            this.fitnessFunction = fitnessFunction;
+
+            BestGenes = new T[dnaSize];
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                Population.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
+            }
+        }
+
     }
 }
