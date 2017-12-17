0944b2c7f74e6187255e244db0f90a4528a4148c
Rikkerdaniel <rik.danka@gmail.com>
2017-12-17 19:39:26 +0100

    feat(Genetic algorithm): Genetic algorithm methods compare dns/new generation/fitnes calculate/chose parent

--- GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
+++ GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -43,6 +43,103 @@ namespace GeneticAlgorithm
                 Population.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
             }
         }
+        public void NewGeneration(int numNewDNA = 0, bool crossoverNewDNA = false)
+        {
+            int finalCount = Population.Count + numNewDNA;
+
+            if (finalCount <= 0)
+            {
+                return;
+            }
+
+            if (Population.Count > 0)
+            {
+                CalculateFitness();
+                Population.Sort(CompareDNA);
+            }
+            newPopulation.Clear();
+
+            for (int i = 0; i < Population.Count; i++)
+            {
+                if (i < Elitism && i < Population.Count)
+                {
+                    newPopulation.Add(Population[i]);
+                }
+                else if (i < Population.Count || crossoverNewDNA)
+                {
+                    DNA<T> parent1 = ChooseParent();
+                    DNA<T> parent2 = ChooseParent();
+
+                    DNA<T> child = parent1.Crossover(parent2);
+
+                    child.Mutate(MutationRate);
+
+                    newPopulation.Add(child);
+                }
+                else
+                {
+                    newPopulation.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
+                }
+            }
+
+            List<DNA<T>> tmpList = Population;
+            Population = newPopulation;
+            newPopulation = tmpList;
+
+            Generation++;
+        }
+
+        private int CompareDNA(DNA<T> a, DNA<T> b)
+        {
+            if (a.Fitness > b.Fitness)
+            {
+                return -1;
+            }
+            else if (a.Fitness < b.Fitness)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void CalculateFitness()
+        {
+            fitnessSum = 0;
+            DNA<T> best = Population[0];
+
+            for (int i = 0; i < Population.Count; i++)
+            {
+                fitnessSum += Population[i].CalculateFitness(i);
+
+                if (Population[i].Fitness > best.Fitness)
+                {
+                    best = Population[i];
+                }
+            }
+
+            BestFitness = best.Fitness;
+            best.Genes.CopyTo(BestGenes, 0);
+        }
+
+        private DNA<T> ChooseParent()
+        {
+            double randomNumber = random.NextDouble() * fitnessSum;
+
+            for (int i = 0; i < Population.Count; i++)
+            {
+                if (randomNumber < Population[i].Fitness)
+                {
+                    return Population[i];
+                }
+
+                randomNumber -= Population[i].Fitness;
+            }
+
+            return null;
+        }
 
     }
 }
