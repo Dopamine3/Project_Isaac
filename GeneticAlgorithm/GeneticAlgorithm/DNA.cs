aae76b12c9762c5bef84c7509d05775a3a6fd170
Rikkerdaniel <rik.danka@gmail.com>
2017-12-17 17:13:47 +0100

    feat(Genetic algorithm): Crossover function /create child genes

--- GeneticAlgorithm/GeneticAlgorithm/DNA.cs
+++ GeneticAlgorithm/GeneticAlgorithm/DNA.cs
@@ -37,7 +37,19 @@ namespace GeneticAlgorithm
             return Fitness;
         }
 
-      
+        public DNA<T> Crossover(DNA<T> otherParent)
+        {
+            DNA<T> child = new DNA<T>(Genes.Length, random, getRandomGene, fitnessFunction, shouldInitGenes: false);
+
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                child.Genes[i] = random.NextDouble() < 0.5 ? Genes[i] : otherParent.Genes[i];
+            }
+
+            return child;
+        }
+
+        
     }
 
 }
